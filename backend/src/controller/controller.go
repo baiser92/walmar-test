e50aa1eacc3db6dfdc527dde4af954558f346a7c
Baiser Roa <bairoa92@gmail.com>
2021-09-01 17:09:13 -0400

    add funciton palindrome and update prices of the products

--- backend/src/controller/controller.go
+++ backend/src/controller/controller.go
@@ -22,8 +22,35 @@ func NewController(rep repository.Repository) Controller {
 }
 func (s *controller) ReadAll(value string, ctx context.Context) ([]*entity.Product, error) {
 	products, err := s.repo.ReadAll(value, ctx)
+
+	if isPalindrome(value) {
+		products = getDiscount(products)
+	}
 	if err != nil {
 		return nil, err
 	}
 	return products, nil
+}
+
+//IsPalindrome function validate is Palindrome
+func isPalindrome(input string) bool {
+	for i := 0; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func getDiscount(products []*entity.Product) []*entity.Product {
+
+	for _, product := range products {
+
+		product.DiscountPrice = product.Price
+		product.Price = product.DiscountPrice / 2
+		product.IsDiscount = true
+	}
+
+	return products
+
 }
